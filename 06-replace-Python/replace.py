c2915a7fac1e63175e9ac660dddcd5c551ec0d2d
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-16 20:11:35 +0530

    Jul 16  20:11:35

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -2,8 +2,13 @@
 # write its equivalent. Specifically, write the function 
 # replace(s1, s2, s3) that returns a string equal to 
 # s1.replace(s2, s3), but again without calling s.replace().
-
+# helloworld123 123 345
 
 def fun_replace(s1, s2, s3):
-	return s1
+	for i in range(len(s1)):
+		if(s1[i] == s2):
+			s2 = s2 + s3
+		else:
+			s2 = s3 + s1[i]
+	return s2
 
