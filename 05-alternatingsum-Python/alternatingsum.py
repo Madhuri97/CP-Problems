a49d198287581c649f81575b28da13b3ffb6f18e
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-18 20:31:14 +0530

    Jul 18  20:31:14

--- 05-alternatingsum-Python/alternatingsum.py
+++ 05-alternatingsum-Python/alternatingsum.py
@@ -3,9 +3,15 @@
 # For example, alternatingSum([5,3,8,4]) returns 6 (that is, 5-3+8-4). If the list is empty, return 0.
 
 
-
+def isEven(i):
+	if(i%2 == 0):
+		return True
+	return False
 
 def fun_alternatingsum(a): 
-	return 0
+	for i in range(len(a)):
+		if(not isEven(i)):
+			a[i] = a[i]*(-1)
+	return sum(a)
 
 
