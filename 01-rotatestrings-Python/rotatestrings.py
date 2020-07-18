716e94ca362bb9e2c1450f524dd58b06a0dc463f
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-18 20:18:03 +0530

    Jul 18  20:18:03

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -4,8 +4,23 @@
 # assert(rotateString('abcd',  1) == 'bcda')
 # assert(rotateString('abcd', -1) == 'dabc')
 
-
+def isNegative(n):
+	if(n < 0):
+		return True
+	return False
 
 def fun_rotatestrings(s, n):
+	temp = ""
+	if(not isNegative(n)):
+		n = n%len(s)
+		temp = s[n:]+s[:n]
+		return temp
+
+	if(isNegative(n)):
+		n = abs(n)
+		n = n%len(s)
+		temp = s[len(s) - n :] + s[:len(s) - n]
+		return temp
+		
 	return s
 
