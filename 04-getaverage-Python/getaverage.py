a2221088e43c5de85a2ad9bb942fbe8fcbdb9ca5
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-18 18:30:38 +0530

    Jul 18  18:30:38

--- 04-getaverage-Python/getaverage.py
+++ 04-getaverage-Python/getaverage.py
@@ -5,10 +5,17 @@
 # should be ignored. If there are no integer values, return 0 (do not crash here).
 # For example, getAverage('13,excused,14,absent') ignores the two 
 # strings and averages 13 and 14 to return 13.5. Also, getAverage('a,b,c') returns 0.
-
-
-
+# [13,]
 
 def fun_getaverage(s): 
-	return 0.0
+	sum = 0 #0
+	sp = s.split(',')
+	for i in sp: #13
+		print(i)
+		if(i == int): #T
+			sum = sum+i #13
+	avg = sum/2
+	return float(avg)
+
+fun_getaverage("13,excused,14,absent")
 
