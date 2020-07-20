210926a7f447a9b67d70a245e88f43cf0ce37322
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-20 12:18:56 +0530

    Jul 20  12:18:56

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -2,14 +2,23 @@
 # and returns True if L does not contain any primes, and False otherwise.
 
 # [[12,4,6]; [8,12,14]; [6,18]]
-def fun_hasnoprimes(l):
-	if(len(l) > 1): #3>1
-		for i in l:  #[9,12]
-			for j in i: #9
-				if(j > 1): #T
-					for k in range(2, len(i)):
-						if(j%k == 0): 
-							return False
+def isprime(n):
+	for i in range(2,n):
+		if(n%1 == 0 and n%i == 0):
+			return False
 		return True
+def fun_hasnoprimes(l):
+	# if(len(l) > 1): #3>1
+		# for i in l:  #[9,12]
+		# 	for j in i: #9
+		# 		if(j > 1): #T
+		# 			for k in range(2, len(i)):
+		# 				if(j%k == 0): 
+							# return False
+	if(len(l) > 1):
+		t = [j for sub in l for j in sub]
+		for i in t:
+			if(isprime(i)):
+				return False
 	return False
 
