5e101027a764884d9348672e663feee7f2814745
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-21 16:15:21 +0530

    Jul 21  16:15:20

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -7,13 +7,14 @@
 # [2,5]], 
 
 def fun_matrixmultiply(m1, m2):
-    if len(m1[0]) != len(m2):
-        print(len(m1[0]), len(m2))
+    # print(len(m1[0]), len(m2))
+    # checked row and column length
+    if len(m1[0]) != len(m2):     
         return None
     res = []
-    for i in range(len(m1)):
+    for i in range(len(m1)): #3
         s = []
-        for j in range(len(m2[0])):
+        for j in range(len(m2[0])): #4
             s.append(0)
         res.append(s)
     for i in range(len(m1)):
@@ -22,8 +23,8 @@ def fun_matrixmultiply(m1, m2):
                 res[i][j] += m1[i][k]*m2[k][j]
     return res
 
-# fun_matrixmultiply([[1,3],[2,4],[2,5]], [[1,3,2,2], [2,4,5,1]])
-fun_matrixmultiply([[1],[2,4],[2,5]], [[1,3,2,2], [2,4,5,1]])
+fun_matrixmultiply([[1,3],[2,4],[2,5]], [[1,3,2,2], [2,4,5,1]])
+# fun_matrixmultiply([[1],[2,4],[2,5]], [[1,3,2,2], [2,4,5,1]])
 # fun_matrixmultiply([[1,3,5],[2,4,6],[2,5,7]], [[1,3,2,2], [2,4,5,1]])
 # fun_matrixmultiply([[1,3],[2,4]], [[1,3,2,2], [2,4,5,1]])
    
