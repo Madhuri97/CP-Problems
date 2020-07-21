63da6328f4dfed80b034e55785631bbbbeccdbf1
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-21 16:08:24 +0530

    Jul 21  16:08:24

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -2,10 +2,13 @@
 # (that we will consider to be matrices) and returns a new 2d list that 
 # is the result of multiplying the two matrices. Return None if the 
 # two matrices cannot be multiplied for any reason.
-
+# [[1,3],   [[1,3,2,2],
+# [2,4],      [2,4,5,1]]
+# [2,5]], 
 
 def fun_matrixmultiply(m1, m2):
-    if(len(m1[0]) != len(m2)):
+    if len(m1[0]) != len(m2):
+        print(len(m1[0]), len(m2))
         return None
     res = []
     for i in range(len(m1)):
@@ -13,18 +16,17 @@ def fun_matrixmultiply(m1, m2):
         for j in range(len(m2[0])):
             s.append(0)
         res.append(s)
-    if(len(m1) != 0 and len(m2) != 0):
-        for i in range(len(m1)):
-            for j in range(len(m2[0])):
-                for k in range(len(m2)):
-                    res[i][j] += m1[i][k]*m2[k][j]
-        for r in res:
-            return r
-    return None
-    # if(len(m1) != 0 or len(m2) != 0): 
-    #     res = [[sum(a*b for a,b in zip(m1_row,m2_col)) for m2_col in zip(*m2)] for m1_row in m1]
-    #     return res
-    # return None
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                res[i][j] += m1[i][k]*m2[k][j]
+    return res
+
+# fun_matrixmultiply([[1,3],[2,4],[2,5]], [[1,3,2,2], [2,4,5,1]])
+fun_matrixmultiply([[1],[2,4],[2,5]], [[1,3,2,2], [2,4,5,1]])
+# fun_matrixmultiply([[1,3,5],[2,4,6],[2,5,7]], [[1,3,2,2], [2,4,5,1]])
+# fun_matrixmultiply([[1,3],[2,4]], [[1,3,2,2], [2,4,5,1]])
+   
 
 
 
