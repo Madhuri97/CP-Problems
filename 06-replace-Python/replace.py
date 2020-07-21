2ae5b9e474f29d9b5547b862577ec33c8a0d200e
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-21 14:00:57 +0530

    Jul 21  14:00:57

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -7,10 +7,12 @@
 # helloworld123 world 345
 
 def fun_replace(s1, s2, s3):
-	for i in range(len(s1)):  #0-15
-		while(i < len(s3)):	  #0-5 1-5 2-5 3-5
-			if(s1[i] == s2[i]): #h=h(T) (T) (T)
-				s1[i] = s3[i] # 3 4 5
-			i = i+1
+	# for i in range(len(s1)):  #0-15
+	# 	while(i < len(s3)):	  #0-5 1-5 2-5 3-5
+	# 		if(s1[i] == s2[i]): #h=h(T) (T) (T)
+	# 			s1[i] = s3[i] # 3 4 5
+	# 		i = i+1
+	# return s1
+	s1 = s1.replace(s2,s3)
 	return s1
 
