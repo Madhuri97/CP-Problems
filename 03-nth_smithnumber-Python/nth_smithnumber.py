ac6c1123de13d0c10b39f12d48607fe33801dfa9
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-22 23:23:03 +0530

    Jul 22  23:23:03

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,11 +7,13 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+import math
+
 def isPrime(n):
     for i in range(2,n):
         if(n%1 == 0 and n%i == 0):
             return False
-        return True
+    return True
 
 def sumOfDigits(n):
     s = 0
@@ -23,10 +25,22 @@ def sumOfDigits(n):
 
 def sumOfFactors(n):
     l = []
-    for i in range(2,n+1):
-        if n%i == 0:
-            l.append(i)
-    return sum(l)
+    while n%2 == 0 and n > 0:
+        l.append(int(2))
+        n = n/2
+    for i in range(3, int(math.sqrt(n))+1,2):
+        while n%i == 0:
+            l.append(int(i))
+            n = n/i
+    if n > 2:
+        l.append(int(n))
+    s = 0
+    for i in l:
+        if len(str(i)) == 1:
+            s = s + i
+        elif len(str(i)) > 1 and i is not n:
+            s = s + sumOfDigits(i)
+    return s
 
 def isSmith(n):
     if sumOfDigits(n) == sumOfFactors(n):
@@ -36,7 +50,7 @@ def isSmith(n):
 
 def fun_nth_smithnumber(n):
     l = []
-    for i in range(1,1000):
+    for i in range(1,500):
         if isSmith(i) and not isPrime(i):
             l.append(i)
     return l[n]
