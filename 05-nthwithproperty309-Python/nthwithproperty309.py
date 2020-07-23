c5f84af4496b4c8f9bba0fba2ceb56bfe0ef1342
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-23 23:07:04 +0530

    Jul 23  23:07:04

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -6,4 +6,26 @@
 
 def nthwithproperty309(n):
 	# Your code goes here
-	pass
+	def isWithProperty309(n):
+		i = 0
+		cnt = 0
+		n = n**5
+		t = n
+		while n > 0:
+			if i != n%10:
+				n = n//10
+			elif i == n%10:
+				cnt += 1
+				n = t
+				i += 1
+				if cnt > 9:
+					return True
+		return False
+
+	i = -1
+	gus = 0
+	while i < n:
+		gus += 1
+		if isWithProperty309(gus):
+			i += 1
+	return gus
