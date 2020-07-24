548504cc60e143a4c12ad6291ab90a46b06edb03
Madhuri <madhiurinaidu1997@msitprogram.net>
2020-07-24 16:52:36 +0530

    Jul 24  16:52:35

--- 05-nth_lychrelnumber-Python/nthlychrelnumber.py
+++ 05-nth_lychrelnumber-Python/nthlychrelnumber.py
@@ -4,7 +4,31 @@
 # 196-algorithm, after the most famous number associated with the process.
 # The first few Lychrel numbers are 196, 295, 394, 493, 592, 689, 691, 788, 790, 879, 887….
 
+def reverse(n):
+	q = 0
+	while (n > 0):
+		r = (n%10)
+		q = (q*10)+r
+		n = n // 10
+	return q
+
+def isPalindrome(n):
+	if n == reverse(n):
+		return True
+	else:
+		return False
+
+def isLychrelNumber(n):
+	for i in range(1,51):
+		n = n + reverse(n)
+		if (isPalindrome(n)):
+			return False
+	return True
 
 def nthlychrelnumbers(n):
 	# your code goes here
-	pass
+	l = []
+	for i in range(5000):
+		if isLychrelNumber(i):
+			l.append(i)
+	return l[n-1]
